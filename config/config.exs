93f8a865ae12458faa93aaf1a01190a4a35a20e3
kyfrostbyte <94023660+kyfrostbyte@users.noreply.github.com>
2025-04-07 17:14:55 -0600

    changed db config info

--- config/config.exs
+++ config/config.exs
@@ -2,13 +2,12 @@ import Config
 config :pool_proj, PoolProj.Repo,
   username: "aaron",
   password: "password",
-  database: "defaultdb",
+  database: "pool_proj",
   hostname: "24.199.108.23",
   port: 26257,
   pool_size: 10,
   ssl: false,
-  migration_lock: nil 
+  migration_lock: nil
 
 config :pool_proj,
   ecto_repos: [PoolProj.Repo]
-
